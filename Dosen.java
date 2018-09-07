56a2c9c2dd6d718887d1a73dbc3ab0836f562a48
rismantiamalia <rismantiamalia@gmail.com>
2018-09-07 15:33:17 +0700

    Initial Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,27 @@
+
+public class Dosen {
+
+	String nama;
+	int nik;
+	int lamaMengajar;
+	
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public int getNik() {
+		return nik;
+	}
+	public void setNik(int nik) {
+		this.nik = nik;
+	}
+	public int getLamaMengajar() {
+		return lamaMengajar;
+	}
+	public void setLamaMengajar(int lamaMengajar) {
+		this.lamaMengajar = lamaMengajar;
+	}
+	
+}
