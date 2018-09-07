b28d8883de36c42ebdc7f9356d98f8271087807b
rismantiamalia <rismantiamalia@gmail.com>
2018-09-07 15:20:20 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,27 @@
+
+public class Mahasiswa {
+	
+	String nama;
+	int npm;
+	int umur;
+	
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public int getNpm() {
+		return npm;
+	}
+	public void setNpm(int npm) {
+		this.npm = npm;
+	}
+	public int getUmur() {
+		return umur;
+	}
+	public void setUmur(int umur) {
+		this.umur = umur;
+	}
+
+}
